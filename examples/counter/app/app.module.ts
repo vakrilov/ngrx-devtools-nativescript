0955ef7f6ac3243b976130f854f9255ccdeb2dd5
vakrilov <alexander.vakrilov@telerik.com>
2016-09-08 12:00:55 +0300

    Counter example

---
+++ examples/counter/app/app.module.ts
@@ -0,0 +1,27 @@
+// this import should be first in order to load some required settings (like globals and reflect-metadata)
+import { NativeScriptModule, platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { NativeScriptDevToolsMonitors } from "ngrx-devtools-nativescript";
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+
+import { NgModule } from "@angular/core";
+import { StoreModule } from '@ngrx/store';
+
+import { counterReducer } from "./counter.reducer";
+import { AppComponent } from "./app.component";
+
+
+@NgModule({
+  imports: [
+    NativeScriptModule,
+    NativeScriptDevToolsMonitors,
+    StoreModule.provideStore({ counter: counterReducer }),
+    StoreDevtoolsModule.instrumentStore({
+      // maxAge: 5,
+      // monitor: monitorReducer
+    })
+  ],
+  bootstrap: [AppComponent]
+})
+class AppModule { };
+
+platformNativeScriptDynamic().bootstrapModule(AppModule);
