31ea9839022094a4233460e77ce0217d61ad8b20
vakrilov <alexander.vakrilov@telerik.com>
2017-06-29 18:50:32 +0300

    Update examples

--- examples/counter/app/app.module.ts
+++ examples/counter/app/app.module.ts
@@ -1,6 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { platformNativeScriptDynamic } from "nativescript-angular";
 import { NativeScriptDevToolsMonitors } from "ngrx-devtools-nativescript";
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
--- examples/tic-tac-toe/app/app.module.ts
+++ examples/tic-tac-toe/app/app.module.ts
@@ -1,5 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { NativeScriptModule, platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptDevToolsMonitors } from "ngrx-devtools-nativescript";
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
