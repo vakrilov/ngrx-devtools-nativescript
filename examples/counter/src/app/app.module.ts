ec4f03bbadda31be9dc2d8348248457055e7fcb8
Rob4226 <Rob4226@yahoo.com>
2021-01-12 13:46:03 -0500

    Upgrade counter example project to NativeScript 7

--- examples/counter/src/app/app.module.ts
+++ examples/counter/src/app/app.module.ts
@@ -1,27 +1,29 @@
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NativeScriptDevToolsMonitors } from "ngrx-devtools-nativescript";
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from '@nativescript/angular';
 
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NativeScriptDevToolsMonitors } from 'ngrx-devtools-nativescript';
 
-import { counterReducer } from "./counter.reducer";
 import { AppComponent } from "./app.component";
-
+import { counterReducer } from "./counter.reducer";
 
 @NgModule({
-  imports: [
-    NativeScriptModule,
-    NativeScriptDevToolsMonitors,
-    StoreModule.forRoot({ counter: counterReducer }),
-    StoreDevtoolsModule.instrument()
-  ],
-  declarations: [AppComponent],
-  providers: [],
-  schemas: [
-      NO_ERRORS_SCHEMA
-  ],
-  bootstrap: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        NativeScriptDevToolsMonitors,
+        StoreModule.forRoot({ counter: counterReducer }),
+        StoreDevtoolsModule.instrument()
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
 })
-export class AppModule { };
-
+export class AppModule { }
